ad6c832063e317dc2cdca86f16d7c7e2ba5249f7
eniolaomotee <eniolaomotomi@gmail.com>
2023-09-13 17:14:28 +0100

    Minor changes

--- app/user.py
+++ app/user.py
@@ -16,15 +16,15 @@ def create_person(payload: schemas.PersonBaseSchema, db: Session = Depends(get_d
 
 
 @router.patch('/{user_id}')
-def update_person(personId: str, payload: schemas.PersonBaseSchema, db: Session = Depends(get_db)):
-    person_query = db.query(models.Person).filter(models.Person.id == personId)
+def update_person(user_id: str, payload: schemas.PersonBaseSchema, db: Session = Depends(get_db)):
+    person_query = db.query(models.Person).filter(models.Person.id == user_id)
     db_person = person_query.first()
 
     if not db_person:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'No note with this id: {personId} found')
+                            detail=f'No note with this id: {user_id} found')
     update_data = payload.dict(exclude_unset=True)
-    person_query.filter(models.Person.id == personId).update(update_data,
+    person_query.filter(models.Person.id == user_id).update(update_data,
                                                        synchronize_session=False)
     db.commit()
     db.refresh(db_person)
@@ -32,20 +32,20 @@ def update_person(personId: str, payload: schemas.PersonBaseSchema, db: Session 
 
 
 @router.get('/{user_id}')
-def get_person(personId: str, db: Session = Depends(get_db)):
-    person = db.query(models.Person).filter(models.Person.id == personId).first()
+def get_person(user_id: str, db: Session = Depends(get_db)):
+    person = db.query(models.Person).filter(models.Person.id == user_id).first()
     if not person:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"No person with this id: {personId} found")
+                            detail=f"No person with this id: {user_id} found")
     return {"status": "success", "Person": person}
 
 @router.delete('/{user_id}')
-def delete_person(personId: str, db: Session = Depends(get_db)):
-    person_query = db.query(models.Person).filter(models.Person.id == personId)
+def delete_person(user_id: str, db: Session = Depends(get_db)):
+    person_query = db.query(models.Person).filter(models.Person.id == user_id)
     person = person_query.first()
     if not person:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'No person with this id: {personId} found')
+                            detail=f'No person with this id: {user_id} found')
     person_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
